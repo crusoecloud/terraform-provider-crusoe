ab1d39be43b9868d16973ab54b56d05c91e383e8
andres gutierrez <agutierrez@crusoeenergy.com>
2023-06-20 16:58:17 -0700

    update examples

--- examples/vms/main.tf
+++ examples/vms/main.tf
@@ -11,40 +11,35 @@ locals {
 }
 
 // new VM
-//resource "crusoe_compute_instance" "my_vm" {
-//  name = "my-new-vm"
-//  type = "a40.1x"
-//
-//  ssh_key        = local.my_ssh_key
-//  startup_script = file("startup.sh")
-//
-//  disks = [
-//    // uncomment to attach at startup
-//    // crusoe_storage_disk.data_disk
-//  ]
-//}
-//
-//// attached disk
-//resource "crusoe_storage_disk" "data_disk" {
-//  name = "data-disk"
-//  size = "200GiB"
-//}
-//
-//// firewall rule
-//// note: this allows all ingress over TCP to our VM
-//resource "crusoe_vpc_firewall_rule" "open_fw_rule" {
-//  network           = crusoe_compute_instance.my_vm.network_interfaces[0].network
-//  name              = "testrule-terra"
-//  action            = "allow"
-//  direction         = "ingress"
-//  protocols         = "tcp"
-//  source            = "0.0.0.0/0"
-//  source_ports      = "1-65535"
-//  destination       = crusoe_compute_instance.my_vm.network_interfaces[0].public_ipv4.address
-//  destination_ports = "1-65535"
-//}
+resource "crusoe_compute_instance" "my_vm" {
+  name = "my-new-vm"
+  type = "a40.1x"
 
-resource "crusoe_ib_partition" "p1" {
-  name = "part2"
-  ib_network_id = "d94b3f8a-c5ca-4ec0-8fcb-099070c694de"
+  ssh_key        = local.my_ssh_key
+  startup_script = file("startup.sh")
+
+  disks = [
+    // attached at startup
+     crusoe_storage_disk.data_disk
+  ]
+}
+
+// attached disk
+resource "crusoe_storage_disk" "data_disk" {
+  name = "data-disk"
+  size = "200GiB"
+}
+
+// firewall rule
+// note: this allows all ingress over TCP to our VM
+resource "crusoe_vpc_firewall_rule" "open_fw_rule" {
+  network           = crusoe_compute_instance.my_vm.network_interfaces[0].network
+  name              = "testrule-terra"
+  action            = "allow"
+  direction         = "ingress"
+  protocols         = "tcp"
+  source            = "0.0.0.0/0"
+  source_ports      = "1-65535"
+  destination       = crusoe_compute_instance.my_vm.network_interfaces[0].public_ipv4.address
+  destination_ports = "1-65535"
 }
