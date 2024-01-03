19ebdcfdfbbeafa0a1940b1c0c835d8606eaabd2
Aaron Jeyaraj <ajeyaraj@crusoeenergy.com>
2024-01-03 22:39:44 +0000

    CRUSOE-3202: Add support for Creating/Updating/Deleting VPC Networks and Subnets

---
+++ examples/vpc-networks/main.tf
@@ -0,0 +1,36 @@
+terraform {
+  required_providers {
+    crusoe = {
+      source = "registry.terraform.io/crusoecloud/crusoe"
+    }
+  }
+}
+
+resource "crusoe_vpc_network" "my_vpc_network" {
+  name = "my-new-network"
+  cidr = "10.0.0.0/8"
+}
+
+resource "crusoe_vpc_subnet" "my_vpc_subnet" {
+  name = "my-new-subnet"
+  cidr = "10.0.0.0/16"
+  location = "us-northcentral1-a"
+  network = crusoe_vpc_network.my_vpc_network.id
+}
+
+// firewall rule
+resource "crusoe_vpc_firewall_rule" "open_fw_rule" {
+  network           = crusoe_vpc_network.my_vpc_network.id
+  name              = "example-terraform-rule"
+  action            = "allow"
+  direction         = "ingress"
+  protocols         = "tcp"
+  source            = "0.0.0.0/0"
+  source_ports      = "1-65535"
+  destination       = crusoe_vpc_network.my_vpc_network.cidr
+  destination_ports = "1-65535"
+
+  // It is currently not possible for terraform to create subnets and firewall rules concurrently.
+  // This directive should be specified when creating firewall rules and subnets to avoid failures.
+  depends_on = [crusoe_vpc_subnet.my_vpc_subnet]
+}
