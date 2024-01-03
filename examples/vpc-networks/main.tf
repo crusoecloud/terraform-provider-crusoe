9000ec96481013f886e7cde584f3be5517bbbabd
Aaron Jeyaraj <ajeyaraj@crusoeenergy.com>
2024-01-03 23:20:46 +0000

    CRUSOE-3202: Create Instances with Non-default Subnets

--- examples/vpc-networks/main.tf
+++ examples/vpc-networks/main.tf
@@ -6,6 +6,10 @@ terraform {
   }
 }
 
+locals {
+  my_ssh_key = file("~/.ssh/id_ed25519.pub")
+}
+
 resource "crusoe_vpc_network" "my_vpc_network" {
   name = "my-new-network"
   cidr = "10.0.0.0/8"
@@ -33,4 +37,23 @@ resource "crusoe_vpc_firewall_rule" "open_fw_rule" {
   // It is currently not possible for terraform to create subnets and firewall rules concurrently.
   // This directive should be specified when creating firewall rules and subnets to avoid failures.
   depends_on = [crusoe_vpc_subnet.my_vpc_subnet]
+}
+
+// Create a VM in the new subnet
+resource "crusoe_compute_instance" "my_vm" {
+  name = "my-new-vm"
+  type = "a40.1x"
+  location = "us-northcentral1-a"
+
+  # specify the base image
+  image = "ubuntu20.04:latest"
+
+  ssh_key = local.my_ssh_key
+
+  network_interfaces = [
+      {
+        subnet = crusoe_vpc_subnet.my_vpc_subnet.id
+      }
+    ]
+
 }
