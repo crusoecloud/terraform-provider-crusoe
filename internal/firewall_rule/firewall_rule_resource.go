8be21f08cb34d02765a2650c29c5f5c9dd381fcd
Aaron Jeyaraj <ajeyaraj@crusoeenergy.com>
2024-03-08 14:17:17 -0800

    fix lint

--- internal/firewall_rule/firewall_rule_resource.go
+++ internal/firewall_rule/firewall_rule_resource.go
@@ -3,9 +3,10 @@ package firewall_rule
 import (
 	"context"
 	"fmt"
-	"github.com/hashicorp/terraform-plugin-framework/path"
 	"strings"
 
+
+	"github.com/hashicorp/terraform-plugin-framework/path"
 	"github.com/hashicorp/terraform-plugin-framework/resource"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/planmodifier"
