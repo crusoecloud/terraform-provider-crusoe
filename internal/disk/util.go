2e0ec3cd294446c6080195cb4364951dcf68b5a6
Aaron Jeyaraj <ajeyaraj@crusoeenergy.com>
2024-04-05 11:01:49 -0700

    disks

---
+++ internal/disk/util.go
@@ -0,0 +1,30 @@
+package disk
+
+import (
+	"context"
+	swagger "github.com/crusoecloud/client-go/swagger/v1alpha5"
+	"github.com/crusoecloud/terraform-provider-crusoe/internal/common"
+	"github.com/hashicorp/terraform-plugin-framework/types"
+)
+
+func findDisk(ctx context.Context, client *swagger.APIClient, diskID string) (*swagger.DiskV1Alpha5, string, error) {
+	args := common.FindResourceArgs[swagger.DiskV1Alpha5]{
+		ResourceID:  diskID,
+		GetResource: client.DisksApi.GetDisk,
+		IsResource: func(disk swagger.DiskV1Alpha5, id string) bool {
+			return disk.Id == id
+		},
+	}
+
+	return common.FindResource[swagger.DiskV1Alpha5](ctx, client, args)
+
+}
+
+func diskToTerraformResourceModel(disk *swagger.DiskV1Alpha5, state *diskResourceModel) {
+	state.ID = types.StringValue(disk.Id)
+	state.Name = types.StringValue(disk.Name)
+	state.Location = types.StringValue(disk.Location)
+	state.Type = types.StringValue(disk.Type_)
+	state.Size = types.StringValue(disk.Size)
+	state.SerialNumber = types.StringValue(disk.SerialNumber)
+}
