c4b3c7a0b1b5c4b40f66b8d1508dffad800a1f72
Clement Liaw <cliaw@crusoe.ai>
2025-01-28 14:28:57 -0800

    feat: update kubernetes example tf

--- examples/kubernetes/main.tf
+++ examples/kubernetes/main.tf
@@ -9,30 +9,46 @@ terraform {
 locals {
   # Optional: Add your SSH public key to the created nodes to allow SSH access
   my_ssh_key = file("~/.ssh/id_ed25519.pub")
+  control_plane_version = "1.30.8-cmk.1"
+  worker_version = "1.30.8-cmk.1"
+  location = "us-east1-a"
+  add_ons = [
+    "nvidia_gpu_operator",
+    "nvidia_network_operator",
+    "crusoe_csi",
+  ]
+  worker_count = 2
+  worker_type = "c1a.4x"
 }
 
 resource "crusoe_kubernetes_cluster" "my_cluster" {
-  name = "tf-cluster"
-  version = "1.30"
-  configuration = "ha"
-  location = "us-east1-a"
+  name = "my-tf-cluster"
+  # Set the desired CMK control plane version
+  # See `crusoe kubernetes versions list` for available versions
+  version = local.control_plane_version
+  location = local.location
 
   # Optional: Set cluster/service CIDRs and node CIDR mask size
   # cluster_cidr = "192.168.1.0/24"
   # node_cidr_mask_size = "27"
   # service_cluster_ip_range = "192.168.2.0/24"
+
+  # Optional: Add additional add-ons - see docs for options
+  # add_ons = local.add_ons
 }
 
 resource "crusoe_kubernetes_node_pool" "c1a_nodepool" {
-  name = "tf-c1a-nodepool"
+  name = "my-tf-c1a-nodepool"
   cluster_id = crusoe_kubernetes_cluster.my_cluster.id
-  instance_count = "1"
-  type = "c1a.2x"
+  instance_count = local.worker_count
+  # Optional: Set the desired CMK worker node version
+  # version = local.worker_version
+  type = local.worker_type
   ssh_key = local.my_ssh_key
-  requested_node_labels = {
-    # Optional: Kubernetes Node objects will be labeled with the following key:value pairs
-    # "labelkey" = "labelvalue"
-  }
+  # Optional: Kubernetes Node objects will be labeled with the following key:value pairs
+  # requested_node_labels = {
+  #   "labelkey" = "labelvalue"
+  # }
 }
 
 output "cluster" {
